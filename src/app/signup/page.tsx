825b4074fa78d98330533766f79d1725d2fbb663
tech586 <tech@sourcesys.co>
2026-03-14 17:57:40 +0530

    signup

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -16,15 +16,6 @@ const SignUpPage01 = () => {
 
       <main className="bg-background-3 dark:bg-background-7">
         <SignupHero />
-        <CTAV1
-          className="dark:bg-background-6 bg-white"
-          badgeClass="badge-yellow-v2"
-          badgeText="Get Started"
-          ctaHeading="Build a complete website using the assistance"
-          description="Start your free trial today and see your ideas come to life easily and creatively."
-          btnClass="hover:btn-secondary dark:hover:btn-accent"
-          ctaBtnText="Get started"
-        />
       </main>
    
     </Fragment>
